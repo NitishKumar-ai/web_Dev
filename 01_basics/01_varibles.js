397e3a3be06b9b3e6faa1f0b366d12056c959cf6
Nitish <99980274+NitishKumar-ai@users.noreply.github.com>
2026-04-13 23:38:36 +0530

    add initial variable declarations and data types examples

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,21 @@
+const accountID  = 651561
+let accountEmail = "nitishkumar44470@gmail.com"
+var accountPassowrd = "1234"
+
+accountCity = "Bengaluru"
+let accountState;
+
+// accountID = 2 is not allowed
+
+accountEmail = "nitish@cloud.com"
+accountPassowrod = "5645684"
+accountCity = "Jairpur"
+
+console.log(accountID);
+
+/*
+Prefer not to use var
+because in block scope and functiona; scope
+*/
+
+console.table({accountEmail, accountID , accountPassowrd, accountState})
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+console.log("hitesh");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = true
+let dataStored = 4444444444444444
+let accountPassword = "$asdfdfaf**"
+
+console.table({
+  name: typeof name,
+  age: typeof age,
+  isLoggedIn: typeof isLoggedIn,
+  dataStored: typeof dataStored,
+  accountPassword: typeof accountPassword
+})
