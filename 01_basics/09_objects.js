4223a6afa2e47956fcd81fe1152b83c00c0a07a2
Nitish <99980274+NitishKumar-ai@users.noreply.github.com>
2026-04-14 02:50:49 +0530

    add array and object examples

---
+++ 01_basics/09_objects.js
@@ -0,0 +1,14 @@
+// Objects Literls
+
+const user = {
+    name: "Nitish",
+    "fullName": "nitish kumar",
+    age: 20,
+    height: 5.8,
+    isDibetic:false,
+    lastLoggedIn: ['monday', 'staruday', ]
+}
+
+console.log(user.name);
+console.log(user["fullName"]);
+
