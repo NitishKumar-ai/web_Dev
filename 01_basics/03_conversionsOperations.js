18c4057a2c749da777e26d16d57207fd22dca3d0
Nitish <99980274+NitishKumar-ai@users.noreply.github.com>
2026-04-14 00:15:46 +0530

    add conversions operations example

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,43 @@
+let score = "nitish";
+
+
+let valueInNumber = number(score);
+
+
+let isLoggedIn = 'hitesh';
+let booleanisLoggedIn = boolean(isLoggedIn)
+
+let someNumber = 33
+let stringNumber = number(someNumber)
+
+let str1 = "hello"
+let str2 = 'Nitish'
+let str3 = str1 + str2;
+console.log("1"+ 2);
+console.log(1 + "8");
+console.log("1" + 2 + 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.table({
+    score: typeof score,
+    valueInNumber: typeof valueInNumber,
+    stringNumber: typeof stringNumber,
+
+});
