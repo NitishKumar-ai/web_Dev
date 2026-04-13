19a9ff1117cdd2a3b55e05b24ce1d79b09895df0
Nitish <99980274+NitishKumar-ai@users.noreply.github.com>
2026-04-14 03:03:31 +0530

    add symbol usage and greetings function to user object

--- 01_basics/09_objects.js
+++ 01_basics/09_objects.js
@@ -1,8 +1,10 @@
 // Objects Literls
+const mySym = Symbol("key1")
 
 const user = {
     name: "Nitish",
     "fullName": "nitish kumar",
+    [mySym]: "stark",
     age: 20,
     height: 5.8,
     isDibetic:false,
@@ -11,4 +13,12 @@ const user = {
 
 console.log(user.name);
 console.log(user["fullName"]);
+console.log(user[mySym]);
+console.log(typeof user[mySym]);
 
+
+// type one Citizen : Functions
+user.greetings = function(){
+    console.log("Hello JS user");
+}
+console.log(user.greetings);
