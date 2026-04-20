2c0b8dd6d2bc996b80d5a4786447523600e833c9
Nitish <99980274+NitishKumar-ai@users.noreply.github.com>
2026-04-21 03:04:15 +0530

    add template literal examples for string interpolation and expressions

--- 01_basics/11_modern.js
+++ 01_basics/11_modern.js
@@ -211,7 +211,7 @@ const mathUtils = (() => {
     const add = (a, b) => a + b;
     const subtract = (a, b) => a -b;
     const multiply = (a, b) => a * b;
-    const divide = (a, b) => (b ! == 0 ? a /b : " cannot divided by  zero")
+    const divide = (a, b) => (b !== 0 ? a /b : " cannot divided by  zero")
 })
 
 const {add, multiply} = mathUtils;
---
+++ 01_basics/13_template_literal.js
@@ -0,0 +1,20 @@
+// old way
+const naem = "Arjun1";
+const msg = "Hello," + naem + "!";
+
+// new way
+const name = "Arjun";
+const msg = `Hello ${name}`
+
+// ecpression not just varibles
+const price  = 645685;
+const qty = 3;
+const get = 0.18;
+
+// Any expressiuon inside ${}
+console.log(`items: ${qty}`);
+console.log(`Total ${pri$ce*qty}`);
+console.log(`which GST ${(price*qty* (1+ gst).toFirxed(2))}`);
+console.log(`Discount? ${qty > 2 ? "Yes 5% off" : "No"}`);
+
+// items : 3 total 
